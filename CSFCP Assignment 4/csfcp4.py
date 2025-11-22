97a8647a91a08bc5d0806a6f288fbc95760092a0
daksh-dua30 <coolcoder30@gmail.com>
2025-11-22 19:20:55 +0530

    Update csfcp4.py
    Adding comments

--- CSFCP Assignment 4/csfcp4.py
+++ CSFCP Assignment 4/csfcp4.py
@@ -1,22 +1,28 @@
+# NUMBER GUESSING GAME
+# BY --> DAKSH 
+
 import random
 
-
+# DEFINING THE FUNCTION FOR THE GAME
 def number_guessing_game():
     print("\n-------- NUMBER GUESSING GAME --------\n")
     print("Welcome to the Number Guessing Game!")
     print("I'm thinking of a number between 1 and 100.")
 
+    # RANDOMIZING THE NUMBER TO BE GUESSED
     number_to_guess = random.randint(1, 100)
     attempts = 0
 
+    # USING LOOP TO GIVE MULTIPLE ATTEMPTS TO THE USER
     while True:
         user_input = input("Enter your guess: ")
 
-        # Check if the input is a valid number
+        # CHECK IF THE INPUT IS VALID 
         if user_input.isdigit():
             user_guess = int(user_input)
             attempts += 1
 
+            # ADDING HINTS TO THE GAME TO MAKE IT FEEL INTERACTIVE
             if user_guess < number_to_guess:
                 print("Too low! Try again.")
             elif user_guess > number_to_guess:
@@ -24,10 +30,10 @@ def number_guessing_game():
             else:
                 print(
                     f"Congratulations! You've guessed the correct number {number_to_guess} in {attempts} attempts.")
-                break  # Exit the loop if the guess is correct
+                break  # IF THE GUESS IS RIGHT, THE LOOP BREAKS
         else:
-            print("Please enter a valid number.")
+            print("Please enter a valid number.")  # FOR INVALID INPUTS 
 
 
-# Run the game
+# RUNNING THE GAME
 number_guessing_game()
