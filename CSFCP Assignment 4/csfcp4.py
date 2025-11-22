d030c77165543024a822911eca604dc6c2e9c405
daksh-dua30 <coolcoder30@gmail.com>
2025-11-22 19:39:12 +0530

    Update csfcp4.py
    Adding spacing for the output to look clear

--- CSFCP Assignment 4/csfcp4.py
+++ CSFCP Assignment 4/csfcp4.py
@@ -7,18 +7,19 @@ import random
 
 
 def number_guessing_game():
-    print("\n-------- NUMBER GUESSING GAME --------\n")
-    print("Welcome to the Number Guessing Game!")
+    print("\n\n--------- NUMBER GUESSING GAME ---------\n\n")
+    print("Welcome to the Number Guessing Game!\n")
     print("I'm thinking of a number between 1 and 100.")
 
     # RANDOMIZING THE NUMBER TO BE GUESSED
     number_to_guess = random.randint(1, 100)
     attempts = 0
-    # LIMITING THE NUMBER OF MAXIMUM ATTEMPTS 
+    # LIMITING THE NUMBER OF MAXIMUM ATTEMPTS
     max_attempts = 10
 
     while attempts < max_attempts:
         user_input = input("Enter your guess: ")
+        print("\n")
 
         # CHECK IF THE INPUT IS VALID
         if user_input.isdigit():
@@ -27,20 +28,20 @@ def number_guessing_game():
 
             # ADDING HINTS TO THE GAME TO MAKE IT FEEL INTERACTIVE
             if user_guess < number_to_guess:
-                print("Too low! Try again.")
+                print("Too low! Try again.\n")
             elif user_guess > number_to_guess:
-                print("Too high! Try again.")
+                print("Too high! Try again.\n")
             else:
                 print(
                     f"Congratulations! You've guessed the correct number {number_to_guess} in {attempts} attempts.")
                 break  # IF THE GUESS IS RIGHT, THE LOOP BREAKS
         else:
-            print("Please enter a valid number.")  # FOR INVALID INPUTS 
+            print("Please enter a valid number.\n")  # FOR INVALID INPUTS
 
     if attempts == max_attempts and user_guess != number_to_guess:
         print(
             f"Sorry, you've used all {max_attempts} attempts. The correct number was {number_to_guess}.")
 
 
-# RUN THE GAME
+# RUNNING THE GAME
 number_guessing_game()
